cc5128e673a4011d0e4a18aa73ba32e489ed7615
Mandava-Kamal-09 <99558200+Mandava-Kamal-09@users.noreply.github.com>
2026-01-09 19:48:57 -0700

    Update README, fix imports, add dependencies

---
+++ Fibrinet_APP/src/managers/network/__init__.py
@@ -0,0 +1,9 @@
+"""
+Network management module.
+
+Contains solvers for network mechanics and relaxation.
+"""
+
+from .relaxed_network_solver import RelaxedNetworkSolver
+
+__all__ = ["RelaxedNetworkSolver"]
